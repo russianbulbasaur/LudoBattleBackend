b4dd8fe9a32308e4104995eac59068183e2f1b82
Bulbasaur <bulbasaur@localhost.localdomain>
2024-06-23 19:28:10 +0530

    hi

--- app/Routes/gameRoutes.js
+++ app/Routes/gameRoutes.js
@@ -2,14 +2,16 @@ import express from "express";
 import {GameController} from "../game/gameController.js";
 import {jwtVerify} from "../middleware/jwtAuth.js";
 import {StatusController} from "../statistics/statusController.js";
+import multer from "multer";
 export var gameRouter = express.Router()
 const controller = new GameController();
 const statusController = new StatusController();
+const multerEngine = multer({dest:"uploads/screenshots"});
 gameRouter.use(jwtVerify);
 gameRouter.post("/create",controller.createGame);
 gameRouter.delete("/delete",controller.deleteGame);
 gameRouter.post("/accept",controller.acceptGame);
 gameRouter.post("/send-code",controller.sendGameCode);
-gameRouter.post("/submit",controller.submitResults);
+gameRouter.post("/submit",multerEngine.single('screenshot'),controller.submitResults);
 gameRouter.get("/open",statusController.openGames);
 gameRouter.get("/live",statusController.liveGames);
--- app/game/gameController.js
+++ app/game/gameController.js
@@ -1,4 +1,5 @@
 import {Pool} from "../Database/pool.js";
+import multer from "multer";
 
 export class GameController{
 
@@ -135,7 +136,44 @@ export class GameController{
     }
 
 
-    async submitResults() {
-        return undefined;
+    async submitResults(req,res) {
+        const {user} = req;
+        const {result,game_id} = req.body;
+        const uploadedFilename = req.file.filename;
+        if(!result || !game_id || !uploadedFilename){
+            res.status(400).send("Result,gameid,upload required");
+            return;
+        }
+        let connection = await Pool.getConnection();
+        try{
+            await connection.beginTransaction();
+            let [rows,fields] = await connection.query(
+                `select host_id,player_id,attachments,winner from games where id=? for update`,
+                [game_id]
+            );
+            if(rows[0]['host_id']!==user.id && rows[0]['player_id']!==user.id){
+                res.status(400).send("This user is not a host or a player");
+                return;
+            }
+            let winner = rows[0]['winner'];
+            if(result==="win") {
+                if (winner && winner !== user.id) {
+                    res.status(400).send("same");
+                    return;
+                }
+                winner = user.id;
+            }
+            const attachments = rows[0]['attachments'];
+            attachments[user.id] = uploadedFilename;
+            await connection.query(
+                `update games set attachments=?,status=?,winner=? where id=?`,
+                [attachments,'ended',winner,game_id]
+            );
+            await connection.commit();
+        }catch (e){
+            res.status(400).send(e);
+        }finally {
+            connection.release();
+        }
     }
 }
